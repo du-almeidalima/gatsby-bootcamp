f5eb12fdb0e551ddce02bbd11ef79d0dadb23d90
Eduardo Lima <eduardo.lima@venturus.org.br>
2021-05-17 16:44:42 -0300

    Connect Gatsby to Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+				environment: process.env.CONTENTFUL_ENVIRONMENT
+      }
+    }
   ],
   siteMetadata: {
     author: 'Eduardo Lima',
