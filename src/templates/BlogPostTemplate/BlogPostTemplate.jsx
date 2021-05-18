af2c21379f74901c71fc208bdbe08ae13c7cbde5
Eduardo Lima <eduardo.lima@venturus.org.br>
2021-05-18 18:13:46 -0300

    Load Assets from Contentful

--- src/templates/BlogPostTemplate/BlogPostTemplate.jsx
+++ src/templates/BlogPostTemplate/BlogPostTemplate.jsx
@@ -1,18 +1,54 @@
+import { BLOCKS } from "@contentful/rich-text-types"
+import { graphql } from "gatsby"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 import React from "react"
 import Layout from "../../components/Layout/Layout"
-import { graphql } from "gatsby"
 import * as style from "./BlogPostTemplate.module.scss"
+
+// == UI Components ==
+const blogImage = ({ alt, src }) => {
+  return <img src={src} alt={alt}></img>
+}
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      content {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            fluid(maxWidth: 1000) {
+              src
+              srcSet
+            }
+          }
+        }
+      }
     }
   }
 `
 
+/*
+ * Due to the Rich Text Contentful data type, I needed to install an package,
+ * @contentful/rich-text-react-renderer, in order to parse the json that comes from this field
+ */
+
 const BlogPostTemplate = ({ data }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        return (
+          <pre>
+            <code>{JSON.stringify(node, null, 2)}</code>
+          </pre>
+        )
+      },
+    },
+  }
+
   return (
     <Layout>
       <article className={style.AppPost}>
@@ -20,11 +56,7 @@ const BlogPostTemplate = ({ data }) => {
         <small className={style.date}>
           {data.contentfulBlogPost.publishedDate}
         </small>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: null,
-          }}
-        />
+        {(data.contentfulBlogPost.content, options)}
       </article>
     </Layout>
   )
